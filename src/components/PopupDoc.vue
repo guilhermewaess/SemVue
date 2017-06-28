2b84fd3608e7bbfd5cc4958baa05027a2af4cc91
Guilherme Waess <guilherme.waess@gmail.com>
2017-06-27 23:51:54 -0300

    Start to work on Popup

---
+++ src/components/PopupDoc.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="ui container">
+        <h1 class="ui dividing header">Popup</h1>
+    
+        <p>Popup component, did you noted that semantic-ui has a Popup module that you can use without javascript? But, if you need something more "complex", This is something helpful.</p>
+        <p></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PopupDoc',
+};
+</script>
---
+++ src/semvue/components/Popup.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Popup',
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
