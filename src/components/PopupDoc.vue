365a4acf29aff27a3723079cce453ce23000e853
Guilherme Waess <guilherme.waess@gmail.com>
2017-06-29 22:24:30 -0300

    Fix dropdown width Fix the bad english Publish demo

--- src/components/PopupDoc.vue
+++ src/components/PopupDoc.vue
@@ -2,7 +2,7 @@
     <div class="ui container">
         <h1 class="ui dividing header">Popup</h1>
     
-        <p>Popup component, did you noted that semantic-ui has a Popup module that you can use without javascript? But, if you need something more "complex", This is something helpful.</p>
+        <p>Popup component, did you note that semantic-ui has a Popup module that you can use without javascript? But, if you need something more "complex", This is something helpful.</p>
         <p>
             <a href="https://semantic-ui.com/modules/popup.html"> you can see all documentation here</a>
         </p>
--- src/semvue/components/Dropdown.vue
+++ src/semvue/components/Dropdown.vue
@@ -92,7 +92,5 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.ui.dropdown {
-    width: 100%;
-}
+
 </style>
